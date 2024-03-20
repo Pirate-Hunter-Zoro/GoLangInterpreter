dd8f6f5642f41251e5214b453dbe63293f9cc593
Mikey Ferguson <90787330+Pirate-Hunter-Zoro@users.noreply.github.com>
2024-03-20 12:59:05 -0500

    user friendly parser errors

--- repl/repl.go
+++ repl/repl.go
@@ -10,6 +10,20 @@ import (
 	"monkey/parser"
 )
 
+const MONKEY_FACE = `
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '~---~'
+`
+
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) { // Read until you encounter a new line, take the just read line and pass it to our lexer
@@ -33,13 +47,16 @@ func Start(in io.Reader, out io.Writer) { // Read until you encounter a new line
 			printParseErrors(out, p.Errors())
 			continue
 		}
-		
+
 		io.WriteString(out, program.String())
 		io.WriteString(out, "\n")
 	}
 }
 
 func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
